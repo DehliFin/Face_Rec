b4cd93a58530be767b5e5e64fceb970070759797
jeppe dehli <dehli1212@gmail.com>
2019-10-29 14:28:59 +0100

    stuff

--- Face_Rec/module1.py
+++ Face_Rec/module1.py
@@ -1,4 +1,3 @@
-
 import cv2
 import sys
 #from Face_reg import frame
@@ -16,9 +15,6 @@ while(True):
     imagePath = "frame"
 
     
-    # Create the haar cascade
-
-    
     # Read the image
     image = cv2.imread(imagePath)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -31,26 +27,27 @@ while(True):
         minSize=(30, 30),
         #flags = cv2.cv.CV_HAAR_SCALE_IMAGE
     )
-    
+
     
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        #cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 2)
-        roi_gray = gray[y:y+h, x:x+w]
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 2)
+        roi_gray = gray[y:y+h, x:x+w] # roi = reigon of interest
         roi_color = frame[y:y+h, x:x+w]
 
-        color = (255, 0, 0)
-        stroke = 10
+        color = (255, 0, 0)#blue BGR
+        stroke = 2 #thickness on border
         end_cord_x = x + w
         end_cord_y = y + h
         cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
      
-    #cv2.imshow("Faces found", image)
-    #cv2.waitKey(0)
-    
+    #cv2.imshow("Faces found", frame)    
     # Display the resulting frame
+   
     cv2.imshow('frame',frame)
-    cv2.waitKey(41)
+    
+    cv2.waitKey(41) #24 fps
+
      
 
 cap.release()
